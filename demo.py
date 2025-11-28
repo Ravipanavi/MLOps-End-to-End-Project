5e3fbbd88abf422c285da0284da6943e30b31237
Ravipanavi <ravipanavirs@mail.com>
2025-11-28 21:35:14 +0530

    Pusing the model training code

--- demo.py
+++ demo.py
@@ -10,12 +10,12 @@
 # logging.error("This is an error message.")
 # logging.critical("This is a critical message.")
 
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1 + 'Z'
-except Exception as e:
-    logging.error("Exception occurred", exc_info=True)
-    raise MyException(e, sys)
+# try:
+#     a = 1 + 'Z'
+# except Exception as e:
+#     logging.error("Exception occurred", exc_info=True)
+#     raise MyException(e, sys)
